4cab41e9f13637a68a01baff74307003ecfd2915
devojoyti <dev.devojoyti@gmail.com>
2017-01-26 22:11:58 +0530

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Drowsiness-detection-and-sleep-prevention-system-for-drivers
+# Driver-Sleep-Detector
+
+### Driver drowsiness detection system using Computer Vision
+
+A large chunk of road accidents these days can be related to vehicle driver falling alseep, due to many reasons like excessive fatigue to alcohol intoxication. However, the above cases might not be possible to stop leading to their broad and complex circumstances, however, detecting if the driver is getting asleep and warn him/her beforehand is quite possible. 
+
+In this program, python image processing is used to detect and constantly monitor vehicle driver's face and eyes. If the program detect any signs of drowsiness, like closing of eyes for more than 2 seconds, it will automatically sound an alarm (here, it is only printing text, hoever, it can be easily modified into some warning system). 
+
+##### Requirement's to run 
+
+ - OpenCV installed for python
+ - Numpy for python
+ - If both criteria's are met, then just `python detect.py`, also make sure a camera is connected to PC.
---
+++ detect.py
@@ -0,0 +1,39 @@
+import cv2
+import time
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+cap = cv2.VideoCapture(0)
+
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        if(not len(eyes)):
+            t = 0
+            while(not len(eyes)):
+                t += 1
+                if t > 5:   # driver is sleeping
+                    print "driver is sleeping !\n"
+                    time.sleep(2)
+                    break
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
